828ec22bcea320e9efbf64917117d09f1f8338af
XIAN LI <123456789lixian>
2023-03-31 15:30:20 +0800

    2023.3.31 lixian jiaojie

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"https://zhongkeruitong.top"'
+  // BASE_API: '"https://zhongkeruitong.top"'
+  BASE_API: '"http://172.16.96.30:80"'
 }
